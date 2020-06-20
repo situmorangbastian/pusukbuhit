d8f6a79a832ae2ec7a7e7c73c8198ce872094189
Bastian Paskal Situmorang <bastianpaskal@gmail.com>
2020-06-21 01:04:29 +0700

    fix: connection to database

--- repository/mongo.ts
+++ repository/mongo.ts
@@ -4,9 +4,9 @@ import { MongoClient } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
 const env = config()
 
 const mongo = new MongoClient();
-mongo.connectWithUri(env.MONGO_HOSTNAME_PORT);
+mongo.connectWithUri(env.MONGO_HOST);
 
-const db = mongo.database("situmorangbastian");
+const db = mongo.database(env.MONGO_DB);
 const nodeCollection = db.collection("node");
 
 export { nodeCollection }
