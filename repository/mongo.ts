d424670a0c58efb222f622b46f8f658ae4ff91a5
Bastian Paskal Situmorang <bastianpaskal@gmail.com>
2020-06-20 17:25:53 +0700

    refactor: init connection to database

--- repository/mongo.ts
+++ repository/mongo.ts
@@ -4,9 +4,9 @@ import { MongoClient } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
 const env = config()
 
 const mongo = new MongoClient();
-mongo.connectWithUri("mongodb://"+env.MONGO_HOSTNAME_PORT);
+mongo.connectWithUri(env.MONGO_HOSTNAME_PORT);
 
-const db = mongo.database("pusukbuhit");
+const db = mongo.database("situmorangbastian");
 const nodeCollection = db.collection("node");
 
 export { nodeCollection }
